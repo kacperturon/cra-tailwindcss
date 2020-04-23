8bb946b9a1f9ff9af0207462cd0983f2f25c3536
Kacper Turon <kacperturon@gmail.com>
2020-04-23 22:37:37 +0200

    initial template

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container mx-auto">
+      <header className="bg-gray-800">
+        <h1 className="text-2xl text-white p-3 bg-indigo-600 rounded-full inline-block ml-24">cra-template</h1>
       </header>
     </div>
   );
